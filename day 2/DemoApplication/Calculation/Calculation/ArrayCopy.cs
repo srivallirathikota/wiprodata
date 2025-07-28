7b78f458b731e018a0e7c933d48dad8f8a141b87
SrivalliR <143482027+srivallirathikota@users.noreply.github.com>
2025-07-28 19:50:29 +0530

    Add files via upload

---
+++ day 2/DemoApplication/Calculation/Calculation/ArrayCopy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    internal class ArrayCopy
+    {
+        public void Show()
+        {
+            int[] a = new int[] { 1, 5, 3, 66, 8 };
+            int[] b = a;
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine("ArrayCopy of b: "+b[i]);
+            }
+        }
+        static void Main()
+        {
+            ArrayCopy arrCopy = new ArrayCopy();
+            arrCopy.Show();
+        }
+    }
+}
---
+++ day 2/DemoApplication/Calculation/Calculation/Custom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    internal class Custom
+    {
+        static void Main()
+        {
+            string name;
+            Console.WriteLine("Enter your name");
+            name=Console.ReadLine();
+            Console.WriteLine("name is :" + name);
+        }
+    }
+}
